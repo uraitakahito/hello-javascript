490c59ecce8bb5b74b29f523686d75f6d632db01
Takahito Urai <uraitakahito@gmail.com>
2024-02-03 21:52:35 +0900

    Use locals

--- examples/express/app.js
+++ examples/express/app.js
@@ -4,16 +4,17 @@ const port = 3000
 
 app.set('view engine', 'ejs');
 
+app.locals.data = {
+  items: [
+    {name: "<h1>リンゴ</h1>"},
+    {name: "<h2>バナナ</h2>"},
+    {name: "<h3>スイカ</h3>"}
+  ]
+};
+
 app.get('/', (req, res) => {
-  var data = {
-    items: [
-        {name: "<h1>リンゴ</h1>"},
-        {name: "<h2>バナナ</h2>"},
-        {name: "<h3>スイカ</h3>"}
-    ]
-  };
   // レンダリングを行う
-  res.render("index.ejs", data);
+  res.render("index.ejs", app.locals.data);
 });
 
 app.listen(port, () => {
