8dd33e4a128b92cf6241feaa6fb9f0d106845649
Takahito Urai <uraitakahito@gmail.com>
2024-04-23 21:00:29 +0900

    Add destructuring assignment example.

---
+++ examples/etc/destructuring-assignment.js
@@ -0,0 +1,12 @@
+var a = 1;
+var b = 2;
+var c = {d: 3};
+var d = 3;
+
+console.log(a, b, c, d); // => 1 2 {d: 3} 3
+
+function fullName ({first, last}) {
+  return first + last;
+}
+
+console.log(fullName({ first: 'John', last: 'Doe'})); // => JohnDoe
