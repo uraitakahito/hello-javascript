d82dcf918a19db91fc83438e4650d263133c0b02
Takahito Urai <uraitakahito@gmail.com>
2024-06-24 12:49:46 +0900

    Refactor app.test.js

--- examples/express/test/app.test.js
+++ examples/express/test/app.test.js
@@ -3,21 +3,17 @@ import { describe, expect, it } from 'vitest';
 const request = require('supertest');
 const app = require('../app.cjs');
 
-describe('Get Endpoints (not mocking)', () => {
+describe('Get Endpoints', () => {
   it('/', async () => {
     const res = await request(app).get('/')
     expect(res.status).toEqual(200)
   })
-})
 
-describe('/static', () => {
   it('/static/stanford-bunny.jpg', async () => {
-    const res = await request(app).get('/')
+    const res = await request(app).get('/static/stanford-bunny.jpg')
     expect(res.status).toEqual(200)
   })
-})
 
-describe('/hello-json-1', () => {
   it('/hello-json-1', async () => {
     const res = await request(app).get('/hello-json-1')
     expect(res.body.message).toEqual('Hello, JSON!')
