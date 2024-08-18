17c16a293eb55d81194bc447bdb064f6b9574366
Takahito Urai <uraitakahito@gmail.com>
2024-08-18 18:13:13 +0000

    Add a sample to experiment with run to completion

---
+++ src/run-to-completion.cjs
@@ -0,0 +1,18 @@
+// https://meetup-jp.nhncloud.com/896
+function delay() {
+  for (var i = 0; i < 100000; i++);
+}
+function foo() {
+  delay();
+  bar();
+  console.log('foo!'); // (3)
+}
+function bar() {
+  delay();
+  console.log('bar!'); // (2)
+}
+function baz() {
+  console.log('baz!'); // (4)
+}
+setTimeout(baz, 10); // (1)
+foo();
