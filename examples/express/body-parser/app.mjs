bd58c67bf84971098d46601e0da5c84215aebcfe
Takahito Urai <uraitakahito@gmail.com>
2024-08-19 12:37:27 +0000

    Add a sample program using body-parser

---
+++ examples/express/body-parser/app.mjs
@@ -0,0 +1,35 @@
+// https://www.grepper.com/answers/218377/express+bodyparser+deprecated
+import express from 'express';
+
+export var app = express();
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: false}));
+
+// ※トップページ
+app.get('/', (req, res) => {
+  var msg = 'Input your message';
+  res.render('index.ejs',
+  {
+    title: 'Index',
+    content: msg,
+  });
+});
+
+// ※POST送信の処理
+app.post('/', (req, res) => {
+  var msg = req.body.message;
+  res.render('index.ejs',
+    {
+      title: 'Posted',
+      content: msg,
+    });
+});
