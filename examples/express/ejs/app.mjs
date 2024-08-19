9c30bf311cc7dcba82fec14ba61af8237592dd50
Takahito Urai <uraitakahito@gmail.com>
2024-08-19 16:56:04 +0000

    Change the location where variables are initialized

--- examples/express/ejs/app.mjs
+++ examples/express/ejs/app.mjs
@@ -16,15 +16,16 @@ app.set("views", path.join(__dirname, "views"));
 app.engine('ejs', ejs.renderFile)
 app.engine('html', ejs.renderFile)
 
+app.locals.data = {
+  items: [
+    {name: "<h1>リンゴ</h1>"},
+    {name: "<h2>バナナ</h2>"},
+    {name: "<h3>スイカ</h3>"}
+  ]
+};
+
 app.get("/", function (req, res) {
-  var data = {
-    items: [
-      {name: "<h1>リンゴ</h1>"},
-      {name: "<h2>バナナ</h2>"},
-      {name: "<h3>スイカ</h3>"}
-    ]
-  };
-  res.render("./index.ejs", data);
+  res.render("index.ejs", app.locals.data);
 });
 
 app.get("/html", function (req, res) {
