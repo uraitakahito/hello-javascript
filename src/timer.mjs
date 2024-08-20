2d6684d6828c6d9c020d6448a9324e21093fd0b1
Takahito Urai <uraitakahito@gmail.com>
2024-08-20 15:11:40 +0000

    Add a sample to measure processing time

---
+++ src/timer.mjs
@@ -0,0 +1,18 @@
+var timer = function(name) {
+  var start = performance.now();
+  return {
+    stop: function() {
+      var end  = performance.now();
+      var time = end - start;
+      console.log('Timer:', name, 'finished in', time, 'ms');
+    }
+  }
+};
+
+var t = timer('Some label');
+
+for (let i = 0; i < 1000000; i++) {
+  //
+}
+
+t.stop(); // prints the time elapsed to the js console
