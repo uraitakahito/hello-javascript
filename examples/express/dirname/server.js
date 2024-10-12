52135bf633c0ba796c00fdcb6d72ffeafcd2e523
Takahito Urai <uraitakahito@gmail.com>
2024-10-12 07:46:33 +0000

    Remove unnecessary files

--- examples/express/dirname/server.js
+++
@@ -1,7 +0,0 @@
-// https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
-import { app } from './app.js';
-const port = 3000
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
