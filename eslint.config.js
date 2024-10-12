5198adc839c514638fd5b633d5cee46b012b6e80
Takahito Urai <uraitakahito@gmail.com>
2024-10-12 18:17:49 +0900

    Add eslint-plugin-jest

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,15 @@ import js from '@eslint/js';
 import jsdoc from "eslint-plugin-jsdoc";
 import vitest from "eslint-plugin-vitest";
 
+const testJestConfig = {
+  files: ["test-jest/**/*.test.{js,ts,tsx}"],
+  languageOptions: {
+    globals: {
+      ...globals.jest
+    },
+  },
+}
+
 export default [
   {
     ignores: [
@@ -32,5 +41,6 @@ export default [
       "jsdoc/check-values": "error",
       ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
     }
-  }
+  },
+  testJestConfig,
 ];
