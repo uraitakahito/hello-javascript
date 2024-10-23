1ca48b31f4ffbd75ac3ae6962b32686cfe355722
Takahito Urai <uraitakahito@gmail.com>
2024-10-23 17:07:32 +0000

    Add test for setting URL with GET parameters

--- test-vitest/dom.test.js
+++ test-vitest/dom.test.js
@@ -6,3 +6,12 @@ test("use jsdom in this test file", () => {
 	const element = document.createElement("div");
 	expect(element).not.toBeNull();
 });
+
+const setURL = (url) => jsdom.reconfigure({ url });
+
+describe("Test current location", () => {
+	test("with GET parameter", () => {
+		setURL("https://test.com?foo=bar");
+		expect(window.location.href).toBe("https://test.com/?foo=bar");
+	});
+});
