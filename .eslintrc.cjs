b5fe238af49545582611c4709dd43d71c9c67521
Takahito Urai <uraitakahito@gmail.com>
2024-11-19 23:26:42 +0900

    Add Jest plugin configuration to ESLint for improved testing support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,23 @@ module.exports = {
    */
   extends: "eslint:all",
 
+  /*
+   * https://github.com/jest-community/eslint-plugin-jest
+   */
+  overrides: [
+    {
+      /*
+       * IMPORTANT: `all configuration` is not recommended for production use because it changes with every minor and major version of ESLint. Use it at your own risk.
+       * https://eslint.org/docs/v8.x/use/configure/configuration-files#using-eslintall
+       */
+      extends: ["plugin:jest/all"],
+      files: ["test/**", "test-jest/**"],
+      // You can omit the eslint-plugin- prefix
+      plugins: ["jest"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+    },
+  ],
+
   parserOptions: {
     // https://eslint.org/docs/v8.x/use/configure/language-options#specifying-parser-options
     sourceType: "module",
@@ -21,5 +38,12 @@ module.exports = {
   rules: {
     // https://eslint.org/docs/v8.x/rules/no-ternary
     "no-ternary": "off",
+  },
+
+  // https://github.com/jest-community/eslint-plugin-jest?tab=readme-ov-file#jest-version-setting
+  settings: {
+    jest: {
+      version: require("jest/package.json").version,
+    },
   },
 };
