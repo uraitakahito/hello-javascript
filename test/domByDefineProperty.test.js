247a34f287a2f9523ca30a28f8d7f30103fe2870
Takahito Urai <uraitakahito@gmail.com>
2024-11-20 00:18:24 +0900

    Refactor domByDefineProperty.test.js for consistency and clarity

--- test/domByDefineProperty.test.js
+++ test/domByDefineProperty.test.js
@@ -1,50 +1,54 @@
 const redirectFromExample = () => {
-  const { hostname, search } = window.location;
-  const redirectHostname = hostname === 'example.com' ? 'www.google.com' : hostname;
+  const { hostname, search } = window.location,
+    redirectHostname = hostname === "example.com" ? "www.google.com" : hostname;
   window.location.href = `//${redirectHostname}${search}`;
 };
 
-describe('redirectFromExample', () => {
+describe("redirectFromExample", () => {
   beforeEach(() => {
     global.window = Object.create(null);
-    Object.defineProperty(window, 'location', {
+    Object.defineProperty(window, "location", {
       value: {
-        href: '',
-        pathname: '/',
-        search: '',
-        hostname: '',
+        hostname: "",
+        href: "",
+        pathname: "/",
+        search: "",
       },
       writable: true,
     });
   });
 
-  it('redirect to google.com from example', () => {
-    Object.defineProperty(window, 'location', {
+  it("redirect to google.com from example", () => {
+    expect.assertions(1);
+
+    Object.defineProperty(window, "location", {
       value: {
-        href: 'https://example.com/',
-        pathname: '/',
-        search: '',
-        hostname: 'example.com',
+        hostname: "example.com",
+        href: "https://example.com/",
+        pathname: "/",
+        search: "",
       },
     });
 
     redirectFromExample();
 
-    expect(window.location.href).toEqual('//www.google.com');
+    expect(window.location.href).toBe("//www.google.com");
   });
 
-  it('not redirect to google.com', () => {
-    Object.defineProperty(window, 'location', {
+  it("not redirect to google.com", () => {
+    expect.assertions(1);
+
+    Object.defineProperty(window, "location", {
       value: {
-        href: 'https://www.yahoo.jp/',
-        pathname: '/',
-        search: '?hoge=text',
-        hostname: 'www.yahoo.jp',
+        hostname: "www.yahoo.jp",
+        href: "https://www.yahoo.jp/",
+        pathname: "/",
+        search: "?hoge=text",
       },
     });
 
     redirectFromExample();
 
-    expect(window.location.href).toEqual('//www.yahoo.jp?hoge=text');
+    expect(window.location.href).toBe("//www.yahoo.jp?hoge=text");
   });
 });
