903db82185bb9b5ab75a826c8346bc775813de68
Takahito Urai <uraitakahito@gmail.com>
2024-11-19 22:46:56 +0900

    Add ESLint configuration file for code linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  /*
+   * https://github.com/eslint/eslintrc/blob/main/conf/environments.js
+   */
+  env: {
+    browser: true,
+    es2024: true,
+    node: true,
+  },
+
+  /*
+   * https://eslint.org/docs/v8.x/use/configure/configuration-files#extending-configuration-files
+   */
+  extends: "eslint:all",
+
+  parserOptions: {
+    // https://eslint.org/docs/v8.x/use/configure/language-options#specifying-parser-options
+    sourceType: "module",
+  },
+};
