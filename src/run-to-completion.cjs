a9ce653f1e1435c43dd3c954535e7761319ebfa3
Takahito Urai <uraitakahito@gmail.com>
2024-11-20 02:25:19 +0900

    Refactor functions to improve readability and consistency; replace var with let and update console log messages

--- src/run-to-completion.cjs
+++ src/run-to-completion.cjs
@@ -1,18 +1,27 @@
+/* eslint-disable no-magic-numbers */
+
 // https://meetup-jp.nhncloud.com/896
+
 function delay() {
-  for (var i = 0; i < 100000; i++);
+  for (let i = 0; i < 100000; i += 1) {
+    /* empty */
+  }
 }
+
+function bar() {
+  delay();
+  console.log("bar!"); // (2)
+}
+
 function foo() {
   delay();
   bar();
-  console.log('foo!'); // (3)
+  console.log("foo!"); // (3)
 }
-function bar() {
-  delay();
-  console.log('bar!'); // (2)
-}
+
 function baz() {
-  console.log('baz!'); // (4)
+  console.log("baz!"); // (4)
 }
+
 setTimeout(baz, 10); // (1)
 foo();
