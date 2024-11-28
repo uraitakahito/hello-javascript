2e3a0e89caf52428e281f8524eca98384a069ea1
Takahito Urai <uraitakahito@gmail.com>
2024-11-29 04:21:26 +0900

    ESLint support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // Vitest supports the same extensions for your configuration file as Vite does:
 // .js, .mjs, .cjs, .ts, .cts, .mts.
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
