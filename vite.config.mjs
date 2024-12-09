34126124ef4219679b0d1c81c2a0873c5ae51c9f
Takahito Urai <uraitakahito@gmail.com>
2024-12-10 05:40:25 +0900

    fix: update import statement in vite.config.mjs to use 'vite' instead of 'vitest/config'

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@
 // .js, .mjs, .cjs, .ts, .cts, .mts.
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   // test: {
