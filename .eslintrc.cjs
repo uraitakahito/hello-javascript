e8ca2952c6b2ae8d1a945218f51c5c52329b3e87
Takahito Urai <uraitakahito@gmail.com>
2024-12-26 01:24:47 +0900

    chore: update import/no-extraneous-dependencies rule to include test-jest files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -101,7 +101,7 @@ module.exports = {
     // https://github.com/import-js/eslint-plugin-import/blob/v2.17.2/docs/rules/extensions.md
     'import/extensions': ['error', 'always', { ignorePackages: true }],
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
-    'import/no-extraneous-dependencies': ['error', { devDependencies: ['rollup.*.js'], peerDependencies: false }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['test-jest/*.js', 'rollup.*.js'], peerDependencies: false }],
     // https://eslint.org/docs/v8.x/rules/line-comment-position
     'line-comment-position': 'off',
     // https://eslint.org/docs/v8.x/rules/multiline-comment-style
