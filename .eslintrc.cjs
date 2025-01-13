9f8133d64160bda2c9116f5e6ce4f0d9fd733bbf
Takahito Urai <uraitakahito@gmail.com>
2025-01-13 14:19:55 +0900

    refactor: add sort-imports rule to ESLint configuration and disable it in import-internal-esmodule.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -113,5 +113,7 @@ module.exports = {
     'no-underscore-dangle': ['error', { allow: ['__dirname'] }],
     // https://eslint.org/docs/v8.x/rules/one-var
     'one-var': 'off',
+    // https://eslint.org/docs/v8.x/rules/sort-imports
+    'sort-imports': 'error',
   },
 };
